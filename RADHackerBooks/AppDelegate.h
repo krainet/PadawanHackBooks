e4eb8cb7af40c5e59b0f686b5cf7cd476a091be8
RAMON ALBERTI DANES <firebird@MacBook-Pro-de-RAMON.local>
2015-03-28 10:01:00 +0100

    Initial Commit

---
+++ RADHackerBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RADHackerBooks
+//
+//  Created by RAMON ALBERTI DANES on 28/3/15.
+//  Copyright (c) 2015 Krainet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
